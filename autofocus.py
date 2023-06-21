c249be1851c48f638eeeb1190b4dcb48544c0bc5
Nicole Agila <nagila@espol.edu.ec>
2023-06-21 10:46:59 -0500

    Update autofocus.py

--- autofocus.py
+++ autofocus.py
@@ -1,15 +1,15 @@
-import cv2
+from PIL import Image
+import os
 import sys
-
 
 def metric(img):
     value=0.5
     return value
 
-input = sys.argv[1]
+input = sys.argv[2]
 images=[]
 entries = os.listdir(input)
 for entry in entries:
-    images.append(cv2.imread(entry, 0))
+    images.append(Image.open(input+entry))
 
 print("la imagen resultante es {} y su metrica fue {}".format(entries[0],metric(images[0])))
